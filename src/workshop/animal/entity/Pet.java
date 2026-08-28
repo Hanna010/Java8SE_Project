602371a660083152e6646ada1be24eb23e1eae91
Hanna010 <byan9110@gmail.com>
2026-08-29 01:10:46 +0900

    푸쉬 인증 테스트

--- src/workshop/animal/entity/Pet.java
+++ src/workshop/animal/entity/Pet.java
@@ -1,6 +1,6 @@
 package workshop.animal.entity;
 
-public interface Pet {
+public interface Pet { //인터페이스는 규격. 클래스가 지켜야하는 약속 (규칙)이다.
 	String getName();
 	void setName(String name);
 	public abstract void play();
